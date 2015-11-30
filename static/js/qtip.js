13da2e7ce28a32af53171216f8ce8d0b254968fb
geoffrey <geoffrey_work@outlook.com>
2015-11-30 16:34:22 +0800

    delete

--- static/js/qtip.js
+++
@@ -1,8 +0,0 @@
-var titleContents = ["Pie Chart","Bar Chart","Row Chart","Data Table Chart","Heatmap Chart","Histogram Chart","Composite Chart","Line Chart","Scatter Plot Chart","Boxplot Chart","Area Chart","Bubble Chart"]
-
-$('.customBtnQtip').each(function(i){
-	$(this).qtip({
-		content: titleContents[i]
-	})
-
-})
